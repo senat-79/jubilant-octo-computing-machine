79540c8fc19c77feb87d87f05585c213f5048e88
senat-79 <senat79@gmail.com>
2023-08-17 19:19:02 +0300

    Задание 1

---
+++ seminar_1/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 2
+/*int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine (num1);
+}
+else
+{
+    Console.WriteLine (num2);
+}
+*/
+
+// Задача 4
+/*int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine (num1);
+}
+
+if (num1 < num3)
+{
+    Console.WriteLine (num2);
+}
+
+else
+{
+    Console.WriteLine (num3);
+}
+*/
+
+// Задача 6
+
+/*int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine ("Чётное");
+}
+
+else
+{
+    Console.WriteLine ("Нечётное");
+}
+*/
+
+// Задача 8
+
+int num = int.Parse(Console.ReadLine());
+int chetnum = int.Parse (Console.ReadLine());
+while (num % 2 == 0)
+{
+    if (chetnum == 0)
+{
+    Sistem.Console.WriteLine (chetnum);
+}
+}
