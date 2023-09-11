3e8f5f6b0cafb4c6e6f42da0fe613ae00fd864d8
senat-79 <140819455+senat-79@users.noreply.github.com>
2023-09-11 22:16:13 +0300

    Add files via upload

---
+++ WorkSeminar_5/work1/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] arr = GetRandomArray (4);
+PrintArray(arr);
+EvenNum(arr);
+
+void EvenNum(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+           even = even + 1;
+        }
+    }
+} 
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+       Console.Write(item + ""); 
+    }
+}
+
+int[] GetRandomArray(int length)
+{
+    int [] result = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        result [i] = new Random().Next (99, 999);
+    }
+    return result;
+}
