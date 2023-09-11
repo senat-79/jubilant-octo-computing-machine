8712ece453bf7c0283be23ba85cfd94c8a2a5b96
senat-79 <140819455+senat-79@users.noreply.github.com>
2023-09-11 22:39:45 +0300

    Add files via upload

---
+++ work2/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих 
+на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] arr = GetRandomArray (10);
+PrintArray(arr);
+Console.WriteLine(SumElements(arr));
+
+int SumElements(int[] array)
+{
+    int sum = 0;
+    int i = 0;
+    while (i < array.Length)
+    {
+       sum = sum + array[i];
+       i = i + 2;
+    }
+    return sum;
+} 
+
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+       Console.Write(item + ""); 
+    }
+}
+
+int[] GetRandomArray(int length)
+{
+    int [] result = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        result [i] = new Random().Next (0, 99);
+    }
+    return result;
+}
