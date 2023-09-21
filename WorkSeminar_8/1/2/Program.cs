bde3e988e75337f42f8fd227d121b92ae8c44108
senat-79 <140819455+senat-79@users.noreply.github.com>
2023-09-21 18:08:56 +0300

    Add files via upload

---
+++ WorkSeminar_8/1/2/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+ которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка */
+
+var matrix = GetMatrix();
+PrintMatrix(matrix);
+System.Console.WriteLine(" ");
+SumLine(matrix);
+
+int SumLine(int[,] matrix)
+{
+    int i = 0;
+    int sum = matrix[i,0];
+    for(int l= 1; l < matrix.GetLength(1); l++)
+    {
+        sum += matrix[i,l]; 
+    }
+    return sum;
+}
+
+/*int minSum = 1;
+int sum = SumLine(matrix, 0);
+for(int i = 1; i <matrix.GetLength(0); i++)
+{
+    if(sum > SumLine(matrix, i))
+    {
+        sum = SumLine(matrix, i);
+        minSum = i + 1;
+    }
+}
+*/
+int[,] GetMatrix(int rows = 5, int columns = 5)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i,l] = rand.Next(1, 9);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            Console.Write($"{matrix[i, l]} ");
+        }
+      Console.WriteLine();  
+    }
+}
---
+++ WorkSeminar_8/1/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая 
+упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+var matrix = GetMatrix();
+PrintMatrix(matrix);
+System.Console.WriteLine(" ");
+ChangeNum(matrix);
+
+void ChangeNum(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int l = 0; l < matrix.GetLength(1); l++)
+        {
+            for(int k = 0; k < matrix.GetLength(1)-1; k++)
+            {
+                if(matrix[i,k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] GetMatrix(int rows = 5, int columns = 6)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i,l] = rand.Next(1, 50);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            Console.Write($"{matrix[i, l]} ");
+        }
+      Console.WriteLine();  
+    }
+}
